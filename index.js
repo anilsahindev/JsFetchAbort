bdfa1b713925819d05481fadb8e78f0097ceefc4
Anıl Şahin <anilsahindev@gmail.com>
2020-01-09 19:26:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-let controller = new AbortController(); const abort = () => controller.abort();
-function App() {
-  const [url, setUrl] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+let controller = new AbortController();
+const abort = () => controller.abort();
+const [url, setUrl] = useState();
+const [loading, setLoading] = useState(false);
+const [error, setError] = useState(false);
 
-  const fetchData = () => {
-    controller = new AbortController(); setUrl();
-    setError(false);
-    setLoading(true);
-    return fetch(
-      "./anil.json",
-      { signal: controller.signal } ).then(r => r.blob())
-      .then(blob => setUrl(URL.createObjectURL(blob)))
-      .catch(e => setError(e.message))
-      .finally(() => setLoading(false));
-  };
+const fetchData = () => {
+        controller = new AbortController();
+        setUrl();
+        setError(false);
+        setLoading(true);
+        return fetch(
+                "./anil.json", {
+                    signal: controller.signal
+                }).then(r => r.blob())
+            .then(blob => setUrl(URL.createObjectURL(blob)))
+            .catch(e => setError(e.message))
+            .finally(() => setLoading(false));
